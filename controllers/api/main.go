821ded9be41e2b7a82f04d50717bbf74c578bc9a
jinhyo-dev <jinhyo0850@gmail.com>
2023-01-10 22:50:08 +0900

    feat: main.go modify application struct

--- controllers/api/main.go
+++ controllers/api/main.go
@@ -12,17 +12,27 @@ import (
 const port = 8080
 
 type application struct {
-	DSN    string
-	Domain string
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
 	var app application
 	flag.StringVar(&app.DSN, "dsn", "root:1234@tcp(127.0.0.1:3306)/go_api?charset=utf8mb4&parseTime=True&loc=Local", "MariaDB Connection")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "singing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "singing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "singing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
+
 	conn, err := app.connectDatabase(app.DSN)
-	app.Domain = "example.com"
 	app.DB = &dbrepo.MariaDBRepo{DB: conn}
 	log.Println("Starting application on port", port)
 
